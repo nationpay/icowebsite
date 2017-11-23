107a835575ca70a2e02ea1d2945a496dc7c56bc2
Daniel Tiati (JL) <dondanidang@gmail.com>
2017-11-24 00:34:44 +0200

    remote to local

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -18,8 +18,8 @@ const api = null
 const getApi = token => mkApi({
   getToken: Ru.K(token),
   apiRoute: 'api',
-  rootUrl: 'https://ico.nationpay.io',
-  // rootUrl: 'http://localhost:3051',
+  // rootUrl: 'https://ico.nationpay.io',
+  rootUrl: 'http://localhost:3051',
   versionUrl: Ru.K(''),
   logger: true,
   // xfInput: snakeCaseKeysIfObj
